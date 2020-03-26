ea207f590f98449b671b29a492cce999e9854d50
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2020-03-25 21:40:26 -0700

    lambda functions test2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,5 @@ const fetchLocation = async () =>
     await (await fetch('/.netlify/functions/getCoords')).json()
 
 fetchLocation().then(data =>{
-    let locationResponse = data.json()
     console.log(data)
 })
